7bc532ba217fb516a26c4ccb3752e93276a716ab
shiwani <shiwanipaunikar1@gmail.com>
2023-01-27 12:10:54 -0800

    Added: add node or element program

--- src/com/bridgelabz/linkedlist/LinkedListMain.java
+++ src/com/bridgelabz/linkedlist/LinkedListMain.java
@@ -4,8 +4,9 @@ public class LinkedListMain {
     public static void main(String[] args) {
 
         createSimpleLinkedList();
-
+        addingElements();
     }
+
     public static void createSimpleLinkedList() {
         System.out.println("\n\n===================UC1=====================\n");
 
@@ -23,6 +24,27 @@ public class LinkedListMain {
         linkedlist1.Display();
         System.out.println("***************************************************************");
     }
+    public static void addingElements() {
+        System.out.println("\n==============================UC2============================");
+
+        Linkedlist<Integer> linkedlist2 = new Linkedlist<>();
+        linkedlist2.add(70);
+        System.out.println("after adding 70");
+        linkedlist2.Display();
+        linkedlist2.add(30);
+        System.out.println("after adding 30");
+        linkedlist2.Display();
+        linkedlist2.add(56);
+        System.out.println("after adding 56");
+        linkedlist2.Display();
+
+        System.out.println("\nwhile adding, the new elements will get added in front " +
+                "\nso required output ===> 56,30,70");
+
+        linkedlist2.Display();
+        System.out.println("********************************************************************");
+    }
+
 
 
 
--- src/com/bridgelabz/linkedlist/Linkedlist.java
+++ src/com/bridgelabz/linkedlist/Linkedlist.java
@@ -3,7 +3,18 @@ package com.bridgelabz.linkedlist;
 public class Linkedlist<T> {
     Node<T> head;
     Node<T> tail;
-
+    /**
+     * Method to add the newnode to linkedlist
+     */
+    public void add(T key) {
+        Node<T> node = new Node<T>();
+        node.key = key;
+        node.next = head;
+        head = node;
+    }
+    /**
+     * Method to push the newnode to linkedlist
+     */
     public void push(T key) {
         Node<T> newNode = new Node(key);
         if (head == null) {
@@ -14,6 +25,9 @@ public class Linkedlist<T> {
             head = newNode;
         }
     }
+    /**
+     * Method to display the linkedlist
+     */
     public void Display () {
         Node<T> tempNode = head;
         while (tempNode != null) {
@@ -22,4 +36,6 @@ public class Linkedlist<T> {
         }
         System.out.println();
     }
+
+
 }
